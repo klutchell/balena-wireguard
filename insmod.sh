55f63a9964383b6538d2cf650a94f9aaa73ecab7
Kyle Harding <kmharding@pm.me>
2021-03-07 09:45:50 -0500

    update to balenaOS 2.69.1+rev1 and reload module if required

--- insmod.sh
+++ insmod.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/with-contenv bash
 # shellcheck shell=bash
 
+# dump module info to logs
+modinfo /app/wireguard-linux-compat/src/wireguard.ko || exit 1
+
+target_version="$(modinfo /app/wireguard-linux-compat/src/wireguard.ko | grep ^version: | awk '{print $2}')"
+
 # print version and exit if wireguard module is already loaded
 if lsmod | grep wireguard >/dev/null 2>&1
 then
-	echo -n "wireguard version: "
-	cat /sys/module/wireguard/version
-	exit 0
+	current_version="$(</sys/module/wireguard/version)"
+	[ "$current_version" = "$target_version" ] && exit 0
+	echo "currently loaded wireguard version $current_version is out-of-date"
+	rmmod -v wireguard
 fi
 
 # modprobe dependencies
 modprobe udp_tunnel
 modprobe ip6_udp_tunnel
-
-# dump module info to logs
-modinfo /app/wireguard-linux-compat/src/wireguard.ko
 
 # insert wireguard module and dump dmesg if it fails
 if ! insmod /app/wireguard-linux-compat/src/wireguard.ko
