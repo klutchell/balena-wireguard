292f52186e759c58105f71f60cb3f2ae4da7b2ad
Kyle Harding <kyle@balena.io>
2021-06-18 13:11:22 -0400

    Don't unload existing wireguard modules
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- insmod.sh
+++ insmod.sh
@@ -4,24 +4,10 @@
 # dump module info to logs
 modinfo /app/wireguard-linux-compat/src/wireguard.ko || exit 1
 
-target_version="$(modinfo /app/wireguard-linux-compat/src/wireguard.ko | grep ^version: | awk '{print $2}')"
-
-# print version and exit if wireguard module is already loaded
-if lsmod | grep wireguard >/dev/null 2>&1
-then
-	current_version="$(</sys/module/wireguard/version)"
-	[ "$current_version" = "$target_version" ] && exit 0
-	echo "currently loaded wireguard version $current_version is out-of-date"
-	rmmod -v wireguard
-fi
-
-# modprobe dependencies
+# load required modules
 modprobe udp_tunnel
 modprobe ip6_udp_tunnel
 
-# insert wireguard module and dump dmesg if it fails
-if ! insmod /app/wireguard-linux-compat/src/wireguard.ko
-then
-	dmesg | grep wireguard
-	sleep infinity
-fi
+# load wireguard module and grep dmesg to logs
+insmod /app/wireguard-linux-compat/src/wireguard.ko || true
+dmesg | grep wireguard
