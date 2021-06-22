0f3f3b21ac09229bd32f7c9c4e8111468441ef35
Kyle Harding <kyle@balena.io>
2021-06-22 14:00:38 -0400

    Rename BALENA_HOSTNAME so it can be set via the dashboard
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- init.sh
+++ init.sh
@@ -3,10 +3,10 @@
 set -euo pipefail
 
 # set a hostname for mDNS (default to wireguard.local)
-if [ -n "${BALENA_HOSTNAME}" ]
+if [ -n "${DEVICE_HOSTNAME}" ]
 then
     curl -X PATCH --header "Content-Type:application/json" \
-        --data "{\"network\": {\"hostname\": \"${BALENA_HOSTNAME}\"}}" \
+        --data "{\"network\": {\"hostname\": \"${DEVICE_HOSTNAME}\"}}" \
         "${BALENA_SUPERVISOR_ADDRESS}/v1/device/host-config?apikey=${BALENA_SUPERVISOR_API_KEY}" || true
 fi
 
