d39a8f91ed5d339fdca6906472bdcb90b4b81fd3
Kyle Harding <kyle@balena.io>
2023-01-06 13:51:52 -0500

    Skip module build as balenaOS v2.106.8 includes it by default
    Change-type: major
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,83 +8,20 @@ cleanup() {
 
 trap "cleanup" TERM INT QUIT EXIT
 
-info() {
-    echo "[INFO] $1"
-}
-
-warn() {
-    echo "[WARN] $1"
-}
-
-fatal() {
-    echo "[FATAL] $1"
-    tail -f /dev/null
-}
-
-do_insmod() {
-
-	if lsmod | grep wireguard >/dev/null 2>&1
-	then
-		return 0
-	fi
-
-    if [ ! -f "${module_path}" ]
-    then
-        return 1
-    fi
-
-	modprobe udp_tunnel
-	modprobe ip6_udp_tunnel
-
-    modinfo "${module_path}"
-
-	if ! insmod "${module_path}"
-	then
-		dmesg | grep wireguard
-		return 1
-	else
-		dmesg | grep wireguard
-		return 0
-	fi
+load_kernel_module() {
+    modprobe wireguard
+    dmesg | grep wireguard
 }
 
 config_root=/etc/wireguard
-server_template=/usr/src/app/templates/server.conf
-peer_template=/usr/src/app/templates/peer.conf
-buildmod_cmd=/usr/src/app/buildmod.sh
-module_path=/usr/src/app/wireguard.ko
+server_template=/app/templates/server.conf
+peer_template=/app/templates/peer.conf
 
 server_conf_path="${config_root}"/wg0.conf
 server_key_path="${config_root}"/wg0.key
 server_pub_path="${config_root}"/wg0.pub
 
-if [ -n "${DISABLE_USERSPACE:-}" ]
-then
-    info "Removing wireguard-go userspace module..."
-    rm /usr/bin/wireguard-go 2>/dev/null || true
-fi
-
-if ! do_insmod
-then
-	"${buildmod_cmd}" || warn "Failed to build Wireguard kernel module!"
-fi
-
-if ! do_insmod && [ -n "${DISABLE_USERSPACE:-}" ]
-then
-    fatal "Failed to load kernel module and userspace is disabled!"
-    info "Check your Device Type and OS Version or unset DISABLE_USERSPACE."
-fi
-
-if ! do_insmod && [ -z "${DISABLE_USERSPACE:-}" ]
-then
-    warn "Using universal wireguard-go userspace module, performance may be impacted!"
-    info "This can be disabled by setting DISABLE_USERSPACE to any value."
-fi
-
-if do_insmod
-then
-    info "Using Wireguard kernel module for maximum performance!"
-fi
+load_kernel_module || true
 
 mkdir -p "${config_root}"
 
@@ -94,7 +31,7 @@ then
     SERVER_HOST="$(curl -s icanhazip.com)"
 fi
 
-info "Assigning '${SERVER_HOST}' as host address..."
+echo "Assigning '${SERVER_HOST}' as host address..."
 
 # restrict default file creation permissions
 umask 077
@@ -102,7 +39,7 @@ umask 077
 # generate server keys if required
 if [ ! -f "${server_key_path}" ]
 then
-    info "Generating new keys for server..."
+    echo "Generating new keys for server..."
     wg genkey | tee "${server_key_path}" | wg pubkey > "${server_pub_path}"
 fi
 
@@ -115,7 +52,7 @@ SERVER_ADDRESS="${MINADDR}"
 SERVER_PRIVKEY="$(cat "${server_key_path}")"
 SERVER_PUBKEY="$(cat "${server_pub_path}")"
 
-info "Assigning '${SERVER_ADDRESS}' as server address..."
+echo "Assigning '${SERVER_ADDRESS}' as server address..."
 
 AVAILABLE_IPS="$(./prips.sh "${MINADDR}" "${MAXADDR}")"
 
@@ -144,7 +81,7 @@ do
     # genrate peer keys if required
     if [ ! -f "${peer_key_path}" ]
     then
-        info "Generating new keys for ${peer_id}..."
+        echo "Generating new keys for ${peer_id}..."
         wg genkey | tee "${peer_key_path}" | wg pubkey > "${peer_pub_path}"
     fi
 
@@ -165,9 +102,10 @@ do
 
     if [ -z "${PEER_ADDRESS}" ]
     then
-        fatal "Failed to find unused IP address for ${peer_id}!"
+        echo "Failed to find unused IP address for ${peer_id}!"
+        tail -f /dev/null
     else
-        info "Assigning '${PEER_ADDRESS}' as ${peer_id} address..."
+        echo "Assigning '${PEER_ADDRESS}' as ${peer_id} address..."
     fi
 
     # remove peer address from available addresses
@@ -196,7 +134,7 @@ TUNFILE=/dev/net/tun
 # set file permissions
 chmod 600 "${config_root}"/*
 
-info "Bringing interface wg0 up..."
+echo "Bringing interface wg0 up..."
 wg-quick up wg0
 
 tail -f /dev/null
